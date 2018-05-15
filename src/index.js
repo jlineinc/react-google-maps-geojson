869c8ae9c9048d25be640bba04ef688edd99c99d
Peter Lau <petercslau@gmail.com>
2018-05-15 15:44:20 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GoogleMapsLoader from 'google-maps'
+import GoogleMapInternal from './GoogleMapInternal'
 
-import { toLatLng, latLngBoundsLiteralCenter } from './utils'
-
+export {default as FeatureLayer} from './FeatureLayer'
 
 export const GoogleAPIContext = React.createContext({
   google: null,
@@ -12,7 +12,8 @@ export const GoogleAPIContext = React.createContext({
 
 export class GoogleAPIProvider extends Component {
   static propTypes = {
-    apiKey: PropTypes.string
+    apiKey: PropTypes.string,
+    children: PropTypes.node
   }
 
   state = {
@@ -31,155 +32,6 @@ export class GoogleAPIProvider extends Component {
       <GoogleAPIContext.Provider value={{google: this.state.google}}>
         {this.props.children}
       </GoogleAPIContext.Provider>
-    )
-  }
-}
-
-const defaultStyle = {
-  container: {
-    width: '100%',
-    height: '100%',
-    position: 'relative'
-  },
-  map: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    top: 0
-  }
-}
-
-const DEFAULT_ZOOM = 14;
-
-class GoogleMapInternal extends Component {
-  constructor(props){
-    super(props)
-    this._mapRef = React.createRef();
-  }
-
-  state = {
-    map: null
-  }
-
-  static propTypes = {
-    onLoad: PropTypes.func,
-    onClick: PropTypes.func,
-    onIdle: PropTypes.func,
-    viewPort: PropTypes.object,
-    visible: PropTypes.bool,
-    style: PropTypes.object,
-    containerStyle: PropTypes.object,
-    mapOptions: PropTypes.object // Additional options passed directly to google map
-  }
-
-  static defaultProps = {
-    viewPort : {
-      zoom: 14,
-      center: {
-        lat: 37.774929,
-        lng: -122.419416
-      }
-    },
-    visible: true
-  }
-  
-  componentDidUpdate(prevProps, prevState) {
-    const { viewPort } = this.props
-    const { map } = this.state
-
-
-    if(!prevProps.google && google){ // Google API Loaded
-      this.loadMap()
-    }
-
-    if(!prevState.map && map && this.props.onMapReady){
-      this.props.onMapReady(map)
-    }
-
-    if (map && viewPort && viewPort !== prevProps.viewPort) {
-      if ("bounds" in viewPort) {
-        this.state.map.fitBounds(viewPort.bounds)
-      }
-      else if ("center" in viewPort && "zoom" in viewPort) {
-        map.setCenter(toLatLng(viewPort.center))
-        map.setZoom(viewPort.zoom)
-      }
-      else {
-        throw "ViewPort must specify either bounds or center and zoom. Do not pass viewPort if uncontrolled.";
-      }
-    }
-
-  }
-
-  componentWillUnmount() {
-    // No official way to destroy map
-  }
-
-  loadMap() {
-    const node = this._mapRef.current
-
-    // Must initialize map with center/zoom. If bounds supplied, use default zoom and call fitBounds()
-    // immediate after map is initialized.
-
-    // Bounds literal : {east, north, south, west}
-    const bounds = this.props.viewPort.bounds ? this.props.viewPort.bounds : null
-    const mapOptions = Object.assign({}, this.props.mapOptions, {
-      center: this.props.viewPort.center ? toLatLng(this.props.viewPort.center) : latLngBoundsLiteralCenter(bounds),
-      zoom: this.props.viewPort.zoom ? this.props.viewPort.zoom : DEFAULT_ZOOM // Default zoom
-    });
-
-    console.log(mapOptions)
-
-    // Create Map and run fitBounds if needed
-    const map = new google.maps.Map(node, mapOptions)
-
-    /*
-    if (bounds) {
-      map.fitBounds(bounds);
-    }
-    */
-
-    // Bind click event
-    map.addListener('click', e => {
-      if (this.props.onClick) {
-        this.props.onClick(e)
-      }
-    });
-
-    map.addListener('idle', () => {
-      if(this.props.onIdle){
-        this.props.onIdle(map)
-      }
-    })
-
-    this.setState({ map: map })
-    
-    // Bind onload event
-    if (this.props.onLoad) {
-      this.props.onLoad(map)
-    }
-
-  }
-
-  render() {
-    const style = {...defaultStyle.map, ...this.props.style, 
-      display: this.props.visible ? 'inherit' : 'none'
-    }
-
-    const containerStyles = {...defaultStyle.container, ...this.props.containerStyle}
-
-    var childrenWithProps = React.Children.map(this.props.children, child => child &&
-      React.cloneElement(child, { map: this.state.map, google: this.props.google })
-    );
-
-    return (
-      <div style={containerStyles} className={this.props.className}>
-        <div style={style} ref={this._mapRef}>
-          Loading map...
-          </div>
-        {this.state.map && childrenWithProps}
-      </div>
     )
   }
 }
